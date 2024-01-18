4a862ad2d22cb1d3296d1c218128a0cc543f0697
Kankanya45 <Kankanya45>
2024-01-19 00:29:53 +0700

    update

--- models/SubjectRepository.go
+++ models/SubjectRepository.go
@@ -1,4 +1,3 @@
-// models/SubjectRepository.go
 package models
 
 import (
@@ -6,24 +5,20 @@ import (
 	"gorm.io/gorm"
 )
 
-// SubjectRepository handles database operations related to the Subject model.
 type SubjectRepository struct {
 	Db *gorm.DB
 }
 
-// NewSubjectRepository creates a new instance of SubjectRepository.
 func NewSubjectRepository(db *gorm.DB) *SubjectRepository {
 	return &SubjectRepository{Db: db}
 }
 
-// GetSubjects retrieves all subjects from the database and returns them as JSON.
 func (r *SubjectRepository) GetSubjects(c *gin.Context) {
 	var subjects []Subject
 	r.Db.Find(&subjects)
 	c.JSON(200, subjects)
 }
 
-// GetSubject retrieves a subject from the database based on the given ID and returns it as JSON.
 func (r *SubjectRepository) GetSubject(c *gin.Context) {
 	id := c.Param("id")
 	var subject Subject
@@ -31,7 +26,6 @@ func (r *SubjectRepository) GetSubject(c *gin.Context) {
 	c.JSON(200, subject)
 }
 
-// CreateSubject adds a new subject to the database and returns it as JSON.
 func (r *SubjectRepository) CreateSubject(c *gin.Context) {
 	var newSubject Subject
 	c.BindJSON(&newSubject)
@@ -39,7 +33,6 @@ func (r *SubjectRepository) CreateSubject(c *gin.Context) {
 	c.JSON(200, newSubject)
 }
 
-// UpdateSubject updates an existing subject in the database and returns it as JSON.
 func (r *SubjectRepository) UpdateSubject(c *gin.Context) {
 	id := c.Param("id")
 	var subject Subject
@@ -49,7 +42,6 @@ func (r *SubjectRepository) UpdateSubject(c *gin.Context) {
 	c.JSON(200, subject)
 }
 
-// DeleteSubject deletes a subject from the database based on the given ID and returns a JSON response.
 func (r *SubjectRepository) DeleteSubject(c *gin.Context) {
 	id := c.Param("id")
 	var subject Subject
