e7ccc4f0a38b99d14ef4909462aceb2e637fa6a8
Kankanya45 <Kankanya45>
2024-02-23 08:03:42 +0700

    update -a

--- models/Teacher.go
+++ models/Teacher.go
@@ -3,8 +3,9 @@ package models
 
 import "gorm.io/gorm"
 
+// Teacher represents the structure of your "Teachers" table in the database.
 type Teacher struct {
-	gorm.Model
+	gorm.Model // Gorm's default model that includes fields like ID, CreatedAt, UpdatedAt, and DeletedAt
 
 	FirstName string
 	LastName  string
