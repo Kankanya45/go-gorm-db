ffdad8762455cf26cb4abb8ef5f418b8b969fa01
Kankanya45 <Kankanya45>
2024-02-23 08:04:49 +0700

    -a

--- models/Teacher.go
+++ models/Teacher.go
@@ -3,9 +3,8 @@ package models
 
 import "gorm.io/gorm"
 
-// Teacher represents the structure of your "Teachers" table in the database.
 type Teacher struct {
-	gorm.Model // Gorm's default model that includes fields like ID, CreatedAt, UpdatedAt, and DeletedAt
+	gorm.Model
 
 	FirstName string
 	LastName  string
--- models/TeacherRepository.go
+++ models/TeacherRepository.go
@@ -6,24 +6,20 @@ import (
 	"gorm.io/gorm"
 )
 
-// TeacherRepository handles database operations related to the Teacher model.
 type TeacherRepository struct {
 	Db *gorm.DB
 }
 
-// NewTeacherRepository creates a new instance of TeacherRepository.
 func NewTeacherRepository(db *gorm.DB) *TeacherRepository {
 	return &TeacherRepository{Db: db}
 }
 
-// GetTeachers retrieves all Teachers from the database and returns them as JSON.
 func (r *TeacherRepository) GetTeachers(c *gin.Context) {
 	var teachers []Teacher
 	r.Db.Find(&teachers)
 	c.JSON(200, teachers)
 }
 
-// GetTeacher retrieves a Teacher from the database based on the given ID and returns it as JSON.
 func (r *TeacherRepository) GetTeacher(c *gin.Context) {
 	id := c.Param("id")
 	var teacher Teacher
@@ -31,7 +27,6 @@ func (r *TeacherRepository) GetTeacher(c *gin.Context) {
 	c.JSON(200, teacher)
 }
 
-// CreateTeacher adds a new Teacher to the database and returns it as JSON.
 func (r *TeacherRepository) CreateTeacher(c *gin.Context) {
 	var newTeacher Teacher
 	c.BindJSON(&newTeacher)
@@ -39,7 +34,6 @@ func (r *TeacherRepository) CreateTeacher(c *gin.Context) {
 	c.JSON(200, newTeacher)
 }
 
-// UpdateTeacher updates an existing Teacher in the database and returns it as JSON.
 func (r *TeacherRepository) UpdateTeacher(c *gin.Context) {
 	id := c.Param("id")
 	var teacher Teacher
@@ -49,7 +43,6 @@ func (r *TeacherRepository) UpdateTeacher(c *gin.Context) {
 	c.JSON(200, teacher)
 }
 
-// DeleteTeacher deletes a Teacher from the database based on the given ID and returns a JSON response.
 func (r *TeacherRepository) DeleteTeacher(c *gin.Context) {
 	id := c.Param("id")
 	var teacher Teacher
